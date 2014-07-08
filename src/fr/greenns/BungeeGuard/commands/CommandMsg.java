4ee623bc14f368712515af52e1a2942dbdaf3c8b
Greenns <greennsmail@gmail.com>
2014-07-08 14:38:45 +0200

    Message privé update /tell command added

--- src/fr/greenns/BungeeGuard/commands/CommandMsg.java
+++ src/fr/greenns/BungeeGuard/commands/CommandMsg.java
@@ -15,7 +15,7 @@ public class CommandMsg extends Command {
 
     public CommandMsg(BungeeGuard plugin)
     {
-        super("msg", "bungeeguard.msg", "m", "w");
+        super("msg", "bungeeguard.msg", "m", "w", "tell");
         this.plugin = plugin;
     }
 
