a300718cbc83ff3ee7a2642ccd1a3a7210b3762f
Stornitz <stornitz@gmail.com>
2014-08-13 01:09:36 +0200

    Ajout de la permission "bungeeguard.moremsg", les joueurs la possédant pas ne peuvent pas envoyer de /msg à ceux qui l'ont

--- src/fr/greenns/BungeeGuard/commands/CommandMsg.java
+++ src/fr/greenns/BungeeGuard/commands/CommandMsg.java
@@ -58,32 +58,36 @@ public class CommandMsg extends Command {
 
 		if (args.length >= 1) {
 			if (!args[0].equalsIgnoreCase(p.getName())) {
-				for (ProxiedPlayer pl : BungeeCord.getInstance().getPlayers()) {
-					if (args[0].equalsIgnoreCase(pl.getName())) {
-						String text1 = "";
-						for (int i = 1; i < args.length; i++)
-							text1 = text1 + args[i] + " ";
-
-						String text = text1;
-						pl.sendMessage(new ComponentBuilder("[").color(ChatColor.GRAY).append(p.getName()).color(ChatColor.GREEN).append(" ➠ ").color(ChatColor.GRAY).append("Moi").color(ChatColor.GREEN).append("]").color(ChatColor.GRAY).append(" " + text).create());
-						p.sendMessage(new ComponentBuilder("[").color(ChatColor.GRAY).append("Moi").color(ChatColor.GREEN).append(" ➠ ").color(ChatColor.GRAY).append(pl.getName()).color(ChatColor.GREEN).append("]").color(ChatColor.GRAY).append(" " + text).create());
-
-						
-						plugin.reply.put(p.getName(), pl);
-						plugin.reply.put(pl.getName(), p);
-						for (UUID uuid : plugin.spy) {
-							try {
-								ProxiedPlayer admin = BungeeCord.getInstance().getPlayer(uuid);
-								admin.sendMessage(new ComponentBuilder("[").color(ChatColor.GRAY).append("SPY").color(ChatColor.RED).append("]").color(ChatColor.GRAY).append(p.getName()).append(": /msg ").append(pl.getName() + " " + text).create());
-							} catch (Exception e) {
-
+				ProxiedPlayer pl = plugin.getProxy().getPlayer(args[0]);
+					if (pl != null) {
+						if(!pl.hasPermission("bungeeguard.moremsg") || p.hasPermission("bungeeguard.moremsg")) {
+							String text1 = "";
+							for (int i = 1; i < args.length; i++)
+								text1 = text1 + args[i] + " ";
+	
+							String text = text1;
+							pl.sendMessage(new ComponentBuilder("[").color(ChatColor.GRAY).append(p.getName()).color(ChatColor.GREEN).append(" ➠ ").color(ChatColor.GRAY).append("Moi").color(ChatColor.GREEN).append("]").color(ChatColor.GRAY).append(" " + text).create());
+							p.sendMessage(new ComponentBuilder("[").color(ChatColor.GRAY).append("Moi").color(ChatColor.GREEN).append(" ➠ ").color(ChatColor.GRAY).append(pl.getName()).color(ChatColor.GREEN).append("]").color(ChatColor.GRAY).append(" " + text).create());
+	
+							
+							plugin.reply.put(p.getName(), pl);
+							plugin.reply.put(pl.getName(), p);
+							for (UUID uuid : plugin.spy) {
+								try {
+									ProxiedPlayer admin = BungeeCord.getInstance().getPlayer(uuid);
+									admin.sendMessage(new ComponentBuilder("[").color(ChatColor.GRAY).append("SPY").color(ChatColor.RED).append("]").color(ChatColor.GRAY).append(p.getName()).append(": /msg ").append(pl.getName() + " " + text).create());
+								} catch (Exception e) {
+	
+								}
 							}
 						}
-
-						return;
+						else { 
+							p.sendMessage(new ComponentBuilder("Vous n'avez pas la permission de parler à ce joueur !").color(ChatColor.RED).create());
+						}
 					}
-				}
-				p.sendMessage(new ComponentBuilder("Le joueur que vous chercher a contacter n'est pas en ligne !").color(ChatColor.RED).create());
+					else { 
+						p.sendMessage(new ComponentBuilder("Le joueur que vous chercher a contacter n'est pas en ligne !").color(ChatColor.RED).create());
+					}
 			} else {
 				p.sendMessage(new ComponentBuilder("Vous ne pouvez pas envoyer un message à vous-même !").color(ChatColor.RED).create());
 			}
