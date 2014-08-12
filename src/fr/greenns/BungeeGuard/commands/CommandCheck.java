45472913f6790e4004c05eabaecdbad916b7660f
Stornitz <stornitz@gmail.com>
2014-08-12 02:06:51 +0200

    Remake /check <player>

--- src/fr/greenns/BungeeGuard/commands/CommandCheck.java
+++ src/fr/greenns/BungeeGuard/commands/CommandCheck.java
@@ -1,13 +1,16 @@
 package fr.greenns.BungeeGuard.commands;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import net.md_5.bungee.BungeeCord;
+import java.util.UUID;
+
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 import fr.greenns.BungeeGuard.BungeeGuard;
+import fr.greenns.BungeeGuard.BungeeGuardUtils;
+import fr.greenns.BungeeGuard.utils.Ban;
+import fr.greenns.BungeeGuard.utils.UUIDFetcher;
 
 public class CommandCheck extends Command {
 
@@ -20,72 +23,36 @@ public class CommandCheck extends Command {
 
 	@Override
 	public void execute(CommandSender sender, String[] args) {
-		if (sender instanceof ProxiedPlayer) {
-			ProxiedPlayer p = (ProxiedPlayer) sender;
-
-			if (!p.hasPermission("bungeeguard.check")) {
-				return;
-			}
-		}
-
-		if (args.length == 0) {
-			plugin.utils.msgPluginCommand(sender);
-			return;
-		}
-
-		if (args.length >= 1) {
-			ResultSet res;
-			ResultSet activeBans;
-			String message;
-
-			plugin.sql.open();
-
-			if (plugin.sql.getConnection() == null) {
-				sender.sendMessage(ChatColor.RED
-						+ "[MYSQL] Connection error ...");
-				return;
-			}
-
-			try {
-
-				String safeuuid = BungeeCord.getInstance().getPlayer(args[0])
-						.getUUID().toString().toLowerCase()
-						.replaceAll("'", "\"");
-
-				res = plugin.sql
-						.query("SELECT COUNT(id) as count FROM `BungeeGuard_Ban` WHERE `uuidBanned` = '"
-								+ safeuuid + "'");
-
-				if (res.next()) {
-					message = "Le joueur " + args[0] + " a "
-							+ res.getInt("count") + " ban";
-					activeBans = plugin.sql
-							.query("SELECT COUNT(id) as count FROM `BungeeGuard_Ban` WHERE `uuidBanned` = '"
-									+ safeuuid + "' AND `status` = 1");
-					if (activeBans.next()) {
-						message += " dont " + activeBans.getInt("count")
-								+ " ban encore actif !";
-					}
-					sender.sendMessage("§c" + message);
-				}
-
-			} catch (final SQLException ex) {
-				System.out
-						.println("SQL problem (exception) while checking player bans : "
-								+ ex);
-			}
-
-			finally {
+		if(args.length != 1) {
+			sender.sendMessage(new ComponentBuilder("Usage: /check <player>").color(ChatColor.RED).create());
+		} else {
+			ProxiedPlayer bannedPlayer = plugin.getProxy().getPlayer(args[0]);
+			UUID bannedUUID;
+			if(bannedPlayer == null) {
 				try {
-					if (!plugin.sql.getConnection().isClosed()) {
-						plugin.sql.close();
+					bannedUUID = UUIDFetcher.getUUIDOf(args[0]);
+					if(bannedUUID == null) {
+						sender.sendMessage(new ComponentBuilder("Erreur: Ce joueur n'existe pas.").color(ChatColor.RED).create());
+						return;
 					}
-				} catch (SQLException ex) {
-					System.out.println(ex);
+				} catch (Exception e) {
+					sender.sendMessage(new ComponentBuilder("Erreur lors de la récupération de l'UUID :").color(ChatColor.RED).append(e.getMessage()).color(ChatColor.GRAY).create());
+					return;
 				}
+			} else {
+				bannedUUID = bannedPlayer.getUniqueId();
+			}
+			
+			Ban Ban = BungeeGuardUtils.getBan(bannedUUID);
+			if(Ban == null) {
+				sender.sendMessage(new ComponentBuilder("Le joueur ").color(ChatColor.YELLOW).append(args[0]).color(ChatColor.AQUA).append(" n'est pas banni.").color(ChatColor.YELLOW).create());
+			} else {
+				sender.sendMessage(new ComponentBuilder("Le joueur ").color(ChatColor.YELLOW).append(args[0]).color(ChatColor.AQUA).append(" est banni.").color(ChatColor.YELLOW).create());
+				if(Ban.getReason().equals(null)) sender.sendMessage(new ComponentBuilder("Raison:  ").color(ChatColor.YELLOW).append(Ban.getReason()).color(ChatColor.AQUA).create());
+				if(Ban.getTime() != -1) sender.sendMessage(new ComponentBuilder("Pendant:  ").color(ChatColor.YELLOW).append(BungeeGuardUtils.getDuration(Ban.getTime())).color(ChatColor.AQUA).create());
+				sender.sendMessage(new ComponentBuilder("Par:  ").color(ChatColor.YELLOW).append(Ban.getAdminName()).color(ChatColor.AQUA).create());
 			}
 		}
-
 	}
 
 }
