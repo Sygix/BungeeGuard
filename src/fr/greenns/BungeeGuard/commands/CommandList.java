9ae4487798ccdb0e957d60e05fc67c687fa127a3
Stornitz <stornitz@gmail.com>
2014-08-12 02:10:03 +0200

    ComponentBuilder4Life

--- src/fr/greenns/BungeeGuard/commands/CommandList.java
+++ src/fr/greenns/BungeeGuard/commands/CommandList.java
@@ -4,6 +4,7 @@ import fr.greenns.BungeeGuard.BungeeGuard;
 import net.md_5.bungee.BungeeCord;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.plugin.Command;
 
 /**
@@ -21,10 +22,7 @@ public class CommandList extends Command {
 	@Override
 	public void execute(CommandSender sender, String[] args) {
 		if (args.length == 0) {
-			sender.sendMessage(ChatColor.AQUA + "Il y a actuellement "
-					+ ChatColor.RED
-					+ BungeeCord.getInstance().getPlayers().size()
-					+ ChatColor.AQUA + " joueurs en ligne sur le serveur !");
+			sender.sendMessage(new ComponentBuilder("Il y a actuellement ").color(ChatColor.AQUA).append(""+BungeeCord.getInstance().getPlayers().size()).color(ChatColor.RED).append(" joueurs en ligne sur le serveur !").color(ChatColor.AQUA).create());
 		}
 	}
 }
