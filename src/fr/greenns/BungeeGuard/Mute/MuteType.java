ab9fa5e45d5f0b2a58bb3debc654b57f95a72107
Stornitz <stornitz@gmail.com>
2014-08-17 04:21:28 +0200

    Return bug in chat when trying to talk

--- src/fr/greenns/BungeeGuard/Mute/MuteType.java
+++ src/fr/greenns/BungeeGuard/Mute/MuteType.java
@@ -6,7 +6,7 @@ import net.md_5.bungee.api.ChatColor;
 
 public enum MuteType {
 	NON_PERMANENT(ChatColor.RED + "Vous avez été mute pendant " + ChatColor.AQUA + "+timeStr" + ChatColor.RED + ".", ChatColor.AQUA + "+adminName" + ChatColor.RED + " a mute " + ChatColor.GREEN + "+muteName" + ChatColor.RED + " pendant " + ChatColor.AQUA + "+timeStr" + ChatColor.RED + "."),
-	NON_PERMANENT_W_REASON(ChatColor.RED + "Vous avez été mute pendant " + ChatColor.AQUA + "+timeStr" + ChatColor.RED + " pour: "+'\n'+"+reasonStr", ChatColor.AQUA + "+adminName" + ChatColor.RED +" a mute " +ChatColor.GREEN+ "+muteName" + ChatColor.RED + " pendant "+ChatColor.AQUA+"+timeStr" + ChatColor.RED + " pour: "+ ChatColor.AQUA + "+reasonStr"),
+	NON_PERMANENT_W_REASON(ChatColor.RED + "Vous avez été mute pendant " + ChatColor.AQUA + "+timeStr" + ChatColor.RED + " pour: "+"+reasonStr", ChatColor.AQUA + "+adminName" + ChatColor.RED +" a mute " +ChatColor.GREEN+ "+muteName" + ChatColor.RED + " pendant "+ChatColor.AQUA+"+timeStr" + ChatColor.RED + " pour: "+ ChatColor.AQUA + "+reasonStr"),
 	UNMUTE("", ChatColor.AQUA + "+adminName" + ChatColor.RED + " a démute " + ChatColor.GREEN + "+muteName" + ChatColor.RED + "."),
 	UNMUTE_W_REASON("", ChatColor.AQUA + "+adminName" + ChatColor.RED + " a démute " + ChatColor.GREEN + "+muteName" + ChatColor.RED + " avec la raison: " + ChatColor.AQUA + "+reasonStr" + ChatColor.RED + ".");
 	
