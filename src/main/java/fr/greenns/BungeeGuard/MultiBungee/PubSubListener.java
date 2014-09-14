33dcd0f21219d28c10a96bc85b14dd6ba43163c4
PunKeel <punkeel@me.com>
2014-09-15 01:42:01 +0200

    Ajout des Plugin Messages (Issue #3)

--- src/main/java/fr/greenns/BungeeGuard/MultiBungee/PubSubListener.java
+++ src/main/java/fr/greenns/BungeeGuard/MultiBungee/PubSubListener.java
@@ -1,10 +1,18 @@
 package fr.greenns.BungeeGuard.MultiBungee;
 
+import com.google.common.io.ByteArrayDataInput;
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
 import fr.greenns.BungeeGuard.Main;
 import fr.greenns.BungeeGuard.MultiBungee.PubSub.*;
 import fr.greenns.BungeeGuard.Party.*;
+import net.md_5.bungee.Util;
+import net.md_5.bungee.api.connection.Server;
+import net.md_5.bungee.api.event.PluginMessageEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
+
+import java.util.UUID;
 
 /**
  * Part of fr.greenns.BungeeGuard.MultiBungee (bungeeguard)
@@ -14,9 +22,11 @@ import net.md_5.bungee.event.EventHandler;
  */
 public class PubSubListener implements Listener {
     Main plugin;
+    MultiBungee MB;
 
     public PubSubListener(Main plugin) {
         this.plugin = plugin;
+        this.MB = plugin.getMB();
     }
 
     @EventHandler
@@ -118,5 +128,72 @@ public class PubSubListener implements Listener {
         if (handler.ignoreSelfMessage() && args.length != 0 && args[0].equals(plugin.getMB().getServerId()))
             return;
         handler.handle(channel, message, args);
+    }
+
+    @EventHandler
+    public void onPluginMessageEvent(PluginMessageEvent e) {
+        if (!(e.getSender() instanceof Server))
+            return;
+        if (!e.getTag().equals("UHCGames"))
+            return;
+        Server sender = (Server) e.getSender();
+
+        ByteArrayDataInput in = ByteStreams.newDataInput(e.getData());
+        String subchannel = in.readUTF();
+        ByteArrayDataOutput out = ByteStreams.newDataOutput();
+
+        if (subchannel.equals("ConnectOther")) {
+            String playerName = in.readUTF();
+            String serverName = in.readUTF();
+            plugin.getMB().summon(playerName, serverName, "");
+        }
+
+        if (subchannel.equals("PlayerCount")) {
+            String target = in.readUTF();
+            out.writeUTF("PlayerCount");
+            if (target.equals("ALL")) {
+                out.writeUTF("ALL");
+                out.writeInt(MB.getPlayerCount());
+            } else {
+                out.writeUTF(target);
+                out.writeInt(MB.getPlayersOnServer(target).size());
+            }
+        }
+
+        if (subchannel.equals("PlayerList")) {
+            String target = in.readUTF();
+            out.writeUTF("PlayerList");
+            if (target.equals("ALL")) {
+                out.writeUTF("ALL");
+                out.writeUTF(Util.csv(MB.getPlayersOnline()));
+            } else {
+                out.writeUTF(target);
+                out.writeUTF(Util.csv(MB.getPlayersOnServer(target)));
+            }
+        }
+
+        if (subchannel.equals("UUIDOther")) {
+            String playerName = in.readUTF();
+            out.writeUTF("UUIDOther");
+            out.writeUTF(playerName);
+            out.writeUTF("" + MB.getUuidFromName(playerName));
+        }
+
+        if (subchannel.equals("Message")) {
+            String playerName = in.readUTF();
+            UUID u = MB.getUuidFromName(playerName);
+            MB.sendPlayerMessage(u, in.readUTF());
+        }
+
+        if (subchannel.equals("KickPlayer")) {
+            String playerName = in.readUTF();
+            String reason = in.readUTF();
+            MB.kickPlayer(playerName, reason);
+        }
+
+        byte[] data = out.toByteArray();
+        if (data.length != 0)
+            sender.sendData("UHCGames", data);
+
     }
 }
