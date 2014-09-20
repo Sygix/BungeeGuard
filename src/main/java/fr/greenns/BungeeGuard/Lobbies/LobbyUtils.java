87ab5502c88eb3752f910cf2baba37649c07a0b2
PunKeel <punkeel@me.com>
2014-09-20 15:25:27 +0200

    Fix issue #17, ou pas. à tester.

--- src/main/java/fr/greenns/BungeeGuard/Lobbies/LobbyUtils.java
+++ src/main/java/fr/greenns/BungeeGuard/Lobbies/LobbyUtils.java
@@ -1,36 +1,39 @@
 package fr.greenns.BungeeGuard.Lobbies;
 
+import com.google.common.base.Predicate;
+import com.google.common.collect.Collections2;
 import fr.greenns.BungeeGuard.Main;
 
-import java.util.List;
+import java.util.Collection;
 
 public class LobbyUtils {
 
     public Main plugin;
+    Predicate<Lobby> isOnline = new Predicate<Lobby>() {
+        public boolean apply(Lobby lobby) {
+            return lobby != null && lobby.isOnline();
+        }
+    };
 
     public LobbyUtils(Main plugin) {
         this.plugin = plugin;
     }
 
-    public List<Lobby> getLobbies() {
-        return plugin.lobbys;
-    }
-
     public Lobby getLobby(String servername) {
         for (Lobby Lobby : plugin.lobbys) {
-            if (Lobby.getName().equals(servername)) return Lobby;
+            if (Lobby.getName().equals(servername))
+                return Lobby;
         }
         return null;
     }
 
     public Lobby bestLobbyTarget() {
-        for (int maxPlayers = 0; maxPlayers < 60; maxPlayers += 15) {
-            for (Lobby lobby : plugin.lobbys) {
-                if (lobby != null && lobby.isOnline() && lobby.getTps() >= 12 && lobby.getNbrPlayers() <= maxPlayers) {
-                    return lobby;
-                }
-            }
-        }
+        Collection<Lobby> lobbies = Collections2.filter(plugin.lobbys, isOnline);
+        for (int tps = 20; tps > 0; tps -= 1)
+            for (int maxPlayers = 0; maxPlayers < 60; maxPlayers += 15)
+                for (Lobby lobby : lobbies)
+                    if (lobby.getTps() >= tps && lobby.getNbrPlayers() <= maxPlayers)
+                        return lobby;
         return null;
     }
 }
