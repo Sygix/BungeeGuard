a2b35788685f33d6ad832b8ac5bf380881ece649
PunKeel <punkeel@me.com>
2014-09-20 16:45:44 +0200

    Utilisation des permissions pour autoriser ou non les couleurs et liens dans les MP.

--- src/main/java/fr/greenns/BungeeGuard/commands/CommandMsg.java
+++ src/main/java/fr/greenns/BungeeGuard/commands/CommandMsg.java
@@ -1,5 +1,6 @@
 package fr.greenns.BungeeGuard.commands;
 
+import com.google.common.collect.ObjectArrays;
 import fr.greenns.BungeeGuard.BungeeGuardUtils;
 import fr.greenns.BungeeGuard.Main;
 import fr.greenns.BungeeGuard.Mute.Mute;
@@ -7,6 +8,7 @@ import fr.greenns.BungeeGuard.Mute.MuteType;
 import fr.greenns.BungeeGuard.utils.Permissions;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
@@ -83,20 +85,23 @@ public class CommandMsg extends Command {
                 p.sendMessage(new ComponentBuilder("Vous ne pouvez pas parler a un joueur ignoré.").color(ChatColor.RED).create());
                 return;
             }
-            String text1 = "";
+            String text = "";
             for (int i = 1; i < args.length; i++)
-                text1 = text1 + args[i] + " ";
+                text += args[i] + " ";
 
-            String text = text1;
-            if (p.hasPermission("bungeeguard.colormsg"))
-                text = ChatColor.translateAlternateColorCodes('&', text);
             BungeeGuardUtils.getMB().sendPrivateMessage(p.getName(), receiverUUID, text);
-            p.sendMessage(new ComponentBuilder("[").color(ChatColor.GRAY).append("Moi").color(ChatColor.GREEN).append(" ➠ ").color(ChatColor.GRAY).append(args[0]).color(ChatColor.GREEN).append("]").color(ChatColor.GRAY).append(" " + text).create());
+
+            BaseComponent[] contenu = new ComponentBuilder("[").color(ChatColor.GRAY).append("Moi").color(ChatColor.GREEN).append(" ➠ ").color(ChatColor.GRAY).append(args[0]).color(ChatColor.GREEN).append("]").color(ChatColor.GRAY).append(" ").create();
+
+            if (p.hasPermission("bungeeguard.colormsg"))
+                contenu = ObjectArrays.concat(contenu, TextComponent.fromLegacyText(text), BaseComponent.class);
+            else
+                contenu = ObjectArrays.concat(contenu, new TextComponent(text));
+
+            p.sendMessage(contenu);
 
 
             plugin.reply.put(p.getUniqueId(), args[0]);
-
-
         }
     }
 }
