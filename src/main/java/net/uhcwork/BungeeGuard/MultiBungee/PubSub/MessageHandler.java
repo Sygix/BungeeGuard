5076bd3ddb367cacada0be2ce2e755f1a4b1a251
PunKeel <punkeel@me.com>
2014-10-26 14:32:10 +0100

    Changement couleur spychat -> Fix #56

--- src/main/java/net/uhcwork/BungeeGuard/MultiBungee/PubSub/MessageHandler.java
+++ src/main/java/net/uhcwork/BungeeGuard/MultiBungee/PubSub/MessageHandler.java
@@ -57,7 +57,7 @@ public class MessageHandler {
         for (UUID uuid : plugin.getSpies()) {
             try {
                 admin = ProxyServer.getInstance().getPlayer(uuid);
-                mp = new ComponentBuilder("[").color(ChatColor.GRAY).append("SPY").color(ChatColor.RED).append("] ").color(ChatColor.GRAY).append(sender).append(": /msg ").append(Main.getMB().getNameFromUuid(receiver) + " ").create();
+                mp = new ComponentBuilder("[").color(ChatColor.GRAY).append("SPY").color(ChatColor.RED).append("] ").color(ChatColor.AQUA).append(sender).append(" > /msg ").append(Main.getMB().getNameFromUuid(receiver) + " ").create();
                 admin.sendMessage(ObjectArrays.concat(mp, contenu, BaseComponent.class));
             } catch (Exception ignored) {
             }
