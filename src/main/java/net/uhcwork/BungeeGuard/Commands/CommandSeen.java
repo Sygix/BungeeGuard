9a6fd8ef53a6c8e7a23021946169f178ad3afef4
PunKeel <punkeel@me.com>
2014-11-20 22:58:13 +0100

    Fixes issue #76
    Ajout du /b:seen <pseudo> lobby

--- src/main/java/net/uhcwork/BungeeGuard/Commands/CommandSeen.java
+++ src/main/java/net/uhcwork/BungeeGuard/Commands/CommandSeen.java
@@ -28,25 +28,32 @@ public class CommandSeen extends Command {
 
     @Override
     public void execute(final CommandSender sender, String[] args) {
-        if (args.length == 1) {
+        if (args.length >= 1 && args.length <= 2) {
             final String playerName = args[0];
+            final boolean onlyLobby = (args.length == 2 && args[1].equalsIgnoreCase("lobby"));
             final UUID u = Main.getMB().getUuidFromName(playerName);
             plugin.executePersistenceRunnable(new VoidRunner() {
+                String serverNameCondition = "";
+
                 @Override
                 protected void run() {
                     sender.sendMessage(TextComponent.fromLegacyText(ChatColor.GOLD + " -- " + playerName + " -- "));
-                    long lastOnline = Main.getMB().getLastOnline(u);
-                    if (lastOnline == 0)
-                        sender.sendMessage(TextComponent.fromLegacyText(ChatColor.GREEN + "En ligne."));
-                    else
-                        sender.sendMessage(TextComponent.fromLegacyText("Dernière connexion il y a " + ChatColor.GREEN + DateUtil.formatDateDiff(lastOnline)));
-                    long presence = Long.valueOf(String.valueOf(Base.firstCell("SELECT SUM(TIME_TO_SEC(TIMEDIFF(leaved_at, joined_at))) FROM bungeelitycs WHERE uuid = ? AND leaved_at IS NOT NULL", BungeeLitycs.toBytes(u))));
-                    sender.sendMessage(TextComponent.fromLegacyText("Temps en ligne : " + ChatColor.GREEN + DateUtil.formatDateDiff(System.currentTimeMillis() + 1000 * presence)));
+                    if (!onlyLobby) {
+                        long lastOnline = Main.getMB().getLastOnline(u);
+                        if (lastOnline == 0)
+                            sender.sendMessage(TextComponent.fromLegacyText(ChatColor.GREEN + "En ligne."));
+                        else
+                            sender.sendMessage(TextComponent.fromLegacyText("Dernière connexion il y a " + ChatColor.GREEN + DateUtil.formatDateDiff(lastOnline)));
+                    } else {
+                        serverNameCondition = "AND server_name LIKE 'lobby%'";
+                    }
+                    long presence = Long.valueOf(String.valueOf(Base.firstCell("SELECT SUM(TIME_TO_SEC(TIMEDIFF(leaved_at, joined_at))) FROM bungeelitycs WHERE uuid = ? AND leaved_at IS NOT NULL " + serverNameCondition, BungeeLitycs.toBytes(u))));
+                    sender.sendMessage(TextComponent.fromLegacyText("Temps en ligne " + (onlyLobby ? "(sur lobby)" : "") + ": " + ChatColor.GREEN + DateUtil.formatDateDiff(System.currentTimeMillis() + 1000 * presence)));
 
                 }
             });
         } else {
-            sender.sendMessage(TextComponent.fromLegacyText(ChatColor.RED + "Usage: /b:seen <pseudo>"));
+            sender.sendMessage(TextComponent.fromLegacyText(ChatColor.RED + "Usage: /b:seen <pseudo> [lobby|rien.]"));
         }
     }
 }
