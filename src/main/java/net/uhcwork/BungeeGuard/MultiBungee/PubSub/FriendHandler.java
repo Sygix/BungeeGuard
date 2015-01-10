69027e9cd9b2a1bd484b324bc8780bb34bd62c6c
PunKeel <punkeel@me.com>
2015-01-10 14:49:00 +0100

    Ajout notification d'ajout ami

--- src/main/java/net/uhcwork/BungeeGuard/MultiBungee/PubSub/FriendHandler.java
+++ src/main/java/net/uhcwork/BungeeGuard/MultiBungee/PubSub/FriendHandler.java
@@ -1,8 +1,14 @@
 package net.uhcwork.BungeeGuard.MultiBungee.PubSub;
 
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.uhcwork.BungeeGuard.Main;
+import net.uhcwork.BungeeGuard.Managers.FriendManager;
 import net.uhcwork.BungeeGuard.MultiBungee.PubSubHandler;
 import net.uhcwork.BungeeGuard.MultiBungee.PubSubMessageEvent;
+import net.uhcwork.BungeeGuard.Utils.MyBuilder;
 
 import java.util.UUID;
 
@@ -11,8 +17,17 @@ public class FriendHandler {
     public static void onFriendAdd(Main plugin, PubSubMessageEvent e) {
         UUID userA = UUID.fromString(e.getArg(0));
         UUID userB = UUID.fromString(e.getArg(1));
-        System.out.println("-" + userA + ", " + userB);
         plugin.getFriendManager().addFriend(userA, userB, false);
+
+        ProxiedPlayer p = plugin.getProxy().getPlayer(userB);
+        String userNameA = Main.getMB().getNameFromUuid(userA);
+        p.sendMessage(TextComponent.fromLegacyText("[" + ChatColor.RED + "♥" + ChatColor.WHITE + "] " + ChatColor.GREEN + userNameA + ChatColor.YELLOW + " vient de vous ajouter à sa liste d'amis."));
+        if (plugin.getFriendManager().getFriendship(userA, userB).equals(FriendManager.STATE.MUTUAL))
+            return;
+        MyBuilder mb = new MyBuilder(ChatColor.YELLOW + "Ajoutez le à votre tour ! ");
+        mb.append(ChatColor.AQUA + "/friend add " + ChatColor.ITALIC + userNameA);
+        mb.event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/friend + " + userNameA));
+        p.sendMessage(mb.create());
     }
 
     @PubSubHandler("delfriend")
